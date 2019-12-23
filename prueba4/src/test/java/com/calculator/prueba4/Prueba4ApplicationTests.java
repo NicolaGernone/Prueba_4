e642ba6f01ad2444eba30ee0509dc476e493102d
NicolaGernone <nicola.gernone02@gmail.com>
2019-12-23 09:32:02 +0100

    creación de tests

--- prueba4/src/test/java/com/calculator/prueba4/Prueba4ApplicationTests.java
+++ prueba4/src/test/java/com/calculator/prueba4/Prueba4ApplicationTests.java
@@ -1,10 +1,19 @@
 package com.calculator.prueba4;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
-class Prueba4ApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ServicesResponseTests {
+	
+	@LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
