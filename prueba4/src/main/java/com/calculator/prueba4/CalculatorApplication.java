ce6e1fee955f0c4d8e90a6b2a30f1f9b998f5e72
NicolaGernone <nicola.gernone02@gmail.com>
2019-12-23 06:48:03 +0100

    contrucción main

--- prueba4/src/main/java/com/calculator/prueba4/CalculatorApplication.java
+++ prueba4/src/main/java/com/calculator/prueba4/CalculatorApplication.java
@@ -3,11 +3,14 @@ package com.calculator.prueba4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
-public class Prueba4Application {
+public class CalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Prueba4Application.class, args);
+		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
 }
