238315f352104b47e790bc87a35e3e59a60aa0b2
NicolaGernone <nicola.gernone02@gmail.com>
2019-12-23 06:46:34 +0100

    Construcción de capa controller.

---
+++ prueba4/src/main/java/com/calculator/prueba4/Controller/OperationController.java
@@ -0,0 +1,64 @@
+package com.calculator.prueba4.Controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.calculator.prueba4.Model.OperationModel;
+import com.calculator.prueba4.Services.OperationServices;
+
+import io.corp.calculator.TracerImpl;
+
+@Controller
+public class OperationController extends TracerImpl{
+	
+	//inicializo el model
+	OperationModel<BigDecimal> modelBean = new OperationModel<>();
+	
+	@Autowired
+	private OperationServices operationServices;
+	
+	/**
+	 * map del html para la interface de la calculadora
+	 * @param model
+	 * @param modelBean
+	 * @return
+	 */
+	@RequestMapping("/calculator")
+    private String getCalculatorPage(Model model){
+        model.addAttribute("operationModel",modelBean);
+        return "calculator";
+	}
+	
+	/**
+	 * servicio para suma
+	 * @param modelBean
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/calculator", params="add", method = RequestMethod.POST)
+    private String add(@ModelAttribute("operationModel")  OperationModel modelBean, Model model ){
+        model.addAttribute("result", operationServices.add(modelBean));
+        return "calculator";
+    }
+	
+	/**
+	 * servicio para resta
+	 * @param modelBean
+	 * @param model
+	 * @return
+	 */
+    @RequestMapping(value="/calculator", params="subtract", method = RequestMethod.POST)
+    private String subtract(@ModelAttribute("operationBean")  OperationModel modelBean, Model model ){
+        model.addAttribute("result", operationServices.subtr(modelBean));
+        return "calculator";
+    }
+	
+
+}
+
