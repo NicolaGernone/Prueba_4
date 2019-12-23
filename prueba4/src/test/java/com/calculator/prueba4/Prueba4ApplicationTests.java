8472e722cb31ae2536fbb644f76a73909fcd8627
NicolaGernone <nicola.gernone02@gmail.com>
2019-12-23 10:59:41 +0100

    creación de test para pruebas positivas

--- prueba4/src/test/java/com/calculator/prueba4/Prueba4ApplicationTests.java
+++ prueba4/src/test/java/com/calculator/prueba4/Prueba4ApplicationTests.java
@@ -1,22 +1,52 @@
 package com.calculator.prueba4;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.calculator.prueba4.Model.OperationModel;
+
+import io.corp.calculator.TracerImpl;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ServicesResponseTests {
+class ServicesResponseTests extends TracerImpl{
 	
 	@LocalServerPort
     private int port;
 
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-	@Test
-	void contextLoads() {
+    @Test
+	void ResponseTests() throws URISyntaxException  {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		//construyo la url
+        final String baseUrl = "http://localhost:"+port+"/calculator";
+        
+		URI uri = new URI(baseUrl);
+		
+		//set de las variables que quiero probar
+        OperationModel<Object> model = new OperationModel<>();
+        model.setA("9.00");
+        model.setB("67.98");
+        
+        //set de los eders de la llamada
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-COM-PERSIST", "true");   
+        
+        HttpEntity<OperationModel<Object>> request = new HttpEntity<>(model, headers);
+        
+        ResponseEntity<String> result = restTemplate.postForEntity(uri, request, String.class);
+        
+        //assert para response positiva
+        Assertions.assertEquals(201, result.getStatusCodeValue());
+        
 	}
 
 }
